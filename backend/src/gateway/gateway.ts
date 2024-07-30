46be7a8d486efda2fe45c7a5ed470f952c472225
adw-ith <adwaithraj45654@gmail.com>
2024-07-30 21:07:27 +0530

    socket

---
+++ backend/src/gateway/gateway.ts
@@ -0,0 +1,35 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+})
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log('New client connected: ' + socket.id);
+    });
+  }
+
+  @SubscribeMessage('message')
+  onMessage(@MessageBody() body: any) {
+    console.log('received message', body);
+    this.server.emit('onMessage', {
+      message: body,
+      timestamp: new Date(),
+    });
+  }
+}
