f8865a8533d5b7af94a6eb2a54169ff34387d590
adw-ith <adwaithraj45654@gmail.com>
2024-08-04 21:36:18 +0530

    prisma added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { SocketModule } from './socket/socket.module';
 import { GatewayModule } from './gateway/gateway.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [GatewayModule, SocketModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GatewayModule, SocketModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
