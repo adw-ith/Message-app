67fd779dd285826f58378d21fa8fe9b18058baa1
adw-ith <adwaithraj45654@gmail.com>
2024-08-06 22:37:56 +0530

    modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { SocketModule } from './socket/socket.module';
 import { GatewayModule } from './gateway/gateway.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [GatewayModule, SocketModule, PrismaModule],
+  imports: [GatewayModule, SocketModule, PrismaModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
